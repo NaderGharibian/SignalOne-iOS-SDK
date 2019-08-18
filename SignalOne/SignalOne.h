02e010d3ec49cc13c2e5219fa710cc637a85508f
Mohammad Amini <devel.trash@gmail.com>
2019-08-18 12:55:00 +0430

    Initial Commit

---
+++ SignalOne/SignalOne.h
@@ -0,0 +1,19 @@
+//
+//  SignalOne.h
+//  SignalOne
+//
+//  Created by Mohammad Amini on 5/27/1398 AP.
+//  Copyright © 1398 AP Mohammad Amini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SignalOne.
+FOUNDATION_EXPORT double SignalOneVersionNumber;
+
+//! Project version string for SignalOne.
+FOUNDATION_EXPORT const unsigned char SignalOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignalOne/PublicHeader.h>
+
+
